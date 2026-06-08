4d60102a166316f5b1aa5e9f01ca1f73f4b029b6
Luis Cabral <55815442+LuisCabralS@users.noreply.github.com>
2026-06-08 10:13:21 -0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = Object.freeze({
+  url: "https://bcoluicxtitijodjlecq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjb2x1aWN4dGl0aWpvZGpsZWNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MDU1NjIsImV4cCI6MjA5NjQ4MTU2Mn0.1LaWFmSsgNZGj8rw_HkahDIxZ44-8uEkO_rvvfBv9rI",
+});
